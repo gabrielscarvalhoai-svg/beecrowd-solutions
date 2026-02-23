a5167e49791f4a6a4d7f3711c1b8543789e8b822
Gabriel Souza Carvalho <gabriel.s.carvalho.ai@gmail.com>
2026-02-23 18:48:25 -0300

    Add problem 1003 - Simple Sum (Beginner)

---
+++ beecrowd-python/beginner/1003.py
@@ -0,0 +1,13 @@
+"""
+Beecrowd Problem 1003 - Simple Sum
+Category: Beginner
+"""
+
+
+A = int(input())
+B = int(input())
+
+# Variable name required by the exercise (SUM)
+SOMA = A + B
+
+print(f'SOMA = {SOMA}')
