b215c2c268540a7899cb9c858afd3ff5c2640495
Gabriel Souza Carvalho <gabriel.s.carvalho.ai@gmail.com>
2026-02-24 17:48:42 -0300

    Add problem 1007 - Difference (Beginner)

---
+++ beecrowd-python/beginner/1007.py
@@ -0,0 +1,20 @@
+"""
+Beecrowd Problem 1007 - Difference
+Category: Beginner
+Calculate the difference between the product of A and B and the product of C and D,
+as requested in the exercise.
+"""
+
+
+# Input
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+# Processing
+# DIFERENCA is the Portuguese word for DIFFERENCE
+DIFERENCA = ((A * B) - (C * D))
+
+# Output
+print(f'DIFERENCA = {DIFERENCA}')
